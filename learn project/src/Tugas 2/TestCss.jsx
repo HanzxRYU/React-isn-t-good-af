eab92697b6275d9607c37b90015e64f1c5fa25c4
HANS <151702640+HanzxRYU@users.noreply.github.com>
2024-11-12 13:58:46 +0700

    Nyoba css dan ambil object

---
+++ learn project/src/Tugas 2/TestCss.jsx
@@ -0,0 +1,38 @@
+// Nyoba CSS
+// export default function StyledComponent() {
+//   const customStyle = {
+//     color: "blue",
+//     fontSize: "20px",
+//     border: "1px solid black",
+//     padding: "10px",
+//     borderRadius: "5px",
+//   };
+
+//   return (
+//     <div style={customStyle}>
+//       <h1>Hello, styled world!</h1>
+//     </div>
+//   );
+// }
+
+// import
+// export default function App({}) {
+//   const title = "Coba JSX{}";
+//   const paragraph = "Lorem ipsum dolor sit amet";
+//   const tema = {
+//     paragraph: {
+//       backgroundcolor: "red",
+//       color: "white",
+//     },
+//   };
+
+//   return (
+//     <>
+//       <img src="../public/APT.gif" alt="The Shutdown" style={{ width: "100%" }} />
+//       <h1 style={{ color: "blue", textAlign: "center" }}>{title}</h1>
+//       <p className="testing" style={tema.paragraph}>
+//         {paragraph}
+//       </p>
+//     </>
+//   );
+// }
--- learn project/src/main.jsx
+++ learn project/src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './m2/Test.jsx'
-import App from './m2/App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./Tugas 2/TestCss.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
