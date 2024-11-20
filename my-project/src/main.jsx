eeeeb904918d344c708668d31c1f12d23a376fb3
HANS <151702640+HanzxRYU@users.noreply.github.com>
2024-11-20 12:10:16 +0700

    Undone

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -8,9 +8,12 @@ import WelcomeMessage from "./Tugas 5/Soal-1.jsx";
 import DeliveryStatus from "./Tugas 5/Soal-2.jsx";
 import Notification from "./Tugas 5/Soal-3.jsx";
 import Greeting from "./Tugas 5/Soal-4.jsx";
+import Button from "./Tugas 4/Component/Atom/Button.jsx";
+import Racket from "./Tugas 6/Rendering List/Racket.jsx";
+import Card from "./Tugas 6/Rendering List/Fragments/Card.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Greeting/>
+    <Card/>
   </StrictMode>
 );
