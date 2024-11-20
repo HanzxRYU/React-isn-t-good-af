da1d8dc0675eeb92275c23958a4989f8c02a0b45
HANS <151702640+HanzxRYU@users.noreply.github.com>
2024-11-20 13:57:05 +0700

    Done

--- my-project/src/Tugas 6/Rendering List/Data/data.jsx
+++ my-project/src/Tugas 6/Rendering List/Data/data.jsx
@@ -5,7 +5,7 @@ export const racketData = [
     price: 180.0,
     description:
       "A racket with Rotational Generator System technology for maximum power and improved control.",
-    image: "path/to/image1.jpg",
+    image: "./public/Rackets.png",
   },
   {
     id: 2,
@@ -13,34 +13,38 @@ export const racketData = [
     price: 140.0,
     description:
       "A racket with an aerodynamic design and high flexibility, ideal for intermediate players.",
-    image: "path/to/image2.jpg",
+    image: "./public/Rackets.png",
   },
   {
     id: 3,
-    title: "Victor Thruster K 9900",
+    name: "Victor Thruster K 9900",
     price: 200.0,
-    desc: "A racket with powerful performance and excellent control, perfect for aggressive players.",
-    image: "./images/Rackets.png",
+    description:
+      "A racket with powerful performance and excellent control, perfect for aggressive players.",
+    image: "./public/Rackets.png",
   },
   {
     id: 4,
-    title: "Babolat Satelite Gravity 74",
+    name: "Babolat Satelite Gravity 74",
     price: 160.0,
-    desc: "A lightweight racket with high precision, perfect for fast-paced games and superior control.",
-    image: "./images/Rackets.png",
+    description:
+      "A lightweight racket with high precision, perfect for fast-paced games and superior control.",
+    image: "./public/Rackets.png",
   },
   {
     id: 5,
-    title: "Ashaway Phantom X",
+    name: "Ashaway Phantom X",
     price: 130.0,
-    desc: "A racket with an innovative design and advanced technology, offering comfort and power in every shot.",
-    image: "./images/Rackets.png",
+    description:
+      "A racket with an innovative design and advanced technology, offering comfort and power in every shot.",
+    image: "./public/Rackets.png",
   },
   {
     id: 6,
-    title: "Yonex Astrox 88D Pro",
+    name: "Yonex Astrox 88D Pro",
     price: 150.0,
-    desc: "A top-tier racket for aggressive players, designed to enhance smash speed and power.",
-    image: "./images/Rackets.png",
+    description:
+      "A top-tier racket for aggressive players, designed to enhance smash speed and power.",
+    image: "./public/Rackets.png",
   },
 ];
