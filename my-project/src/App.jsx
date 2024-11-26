4413ade17c013b38d2368aab593df1dd80318d93
HANS <151702640+HanzxRYU@users.noreply.github.com>
2024-11-26 14:04:28 +0700

    up

---
+++ my-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+const users = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+];
+export default function UserList() {
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+}
--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -4,12 +4,13 @@ import "./index.css";
 import Card from "./Tugas 6/Rendering List/Racket/Fragments/Card";
 import Racket from "./Tugas 6/Rendering List/Racket/Fragments/Racket";
 import UserPage from "./Tugas 6/Soal 2/Component/Pages/UserPage";
+import UserList from "./App";
 
 
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <UserPage/>
+    <UserList/>
   </StrictMode>
 );
