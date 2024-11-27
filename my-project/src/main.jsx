8b86abb4aba6c729015c3aaff7527ca3977cb9ae
HANS <151702640+HanzxRYU@users.noreply.github.com>
2024-11-27 13:50:44 +0700

    Update

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -5,12 +5,13 @@ import Card from "./Tugas 6/Rendering List/Racket/Fragments/Card";
 import Racket from "./Tugas 6/Rendering List/Racket/Fragments/Racket";
 import UserPage from "./Tugas 6/Soal 2/Component/Pages/UserPage";
 import UserList from "./App";
+import PreventDefaultExample from "../Tugas 7/Soal-1";
 
 
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <UserList/>
+    <PreventDefaultExample/>
   </StrictMode>
 );
