e91cc32701d45e56b7f58b34e955711abc947fe1
HANS <151702640+HanzxRYU@users.noreply.github.com>
2025-01-21 14:16:32 +0700

    ok

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Apt from "./Tugas 12/Soal-5";
-import CobaAxios from "./Tugas 13/Soal-1";
+import App from "./Tugas 13/Soal-2";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CobaAxios/>
+    <App/>
   </StrictMode>
 );
