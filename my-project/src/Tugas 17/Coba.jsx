ab701c6ac89a75b807587bc7ca0cea124d71aca5
HanzxRYU <151702640+HanzxRYU@users.noreply.github.com>
2025-02-17 09:49:22 +0700

    ok

---
+++ my-project/src/Tugas 17/Coba.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await api.get("/products");
+        setProducts(response.data);
+      } catch (error) {
+        console.error("Error fetching products", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h2>Daftar Produk</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.title} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Nyocot from "./Tugas 16/Soal-1";
+import ProductList from "./Tugas 17/Coba";
 
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Nyocot/>
+    <ProductList/>
   </StrictMode>
 );
